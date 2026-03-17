07f25a5fcb78e3bd677fe063e5b780d47b05941a
amtrevlyn <musicijlai@gmail.com>
2026-03-17 11:30:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
-const CACHE_NAME = 'sanctuary-v1';
-const assets = [
-  './',
-  './index.html',
-  'https://cdn-icons-png.flaticon.com/512/2589/2589175.png'
+const CACHE_NAME = 'sanctuary-cache-v1';
+const assetsToCache = [
+    './',
+    './index.html',
+    'https://cdn-icons-png.flaticon.com/512/2589/2589175.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assetsToCache);
+        })
+    );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
